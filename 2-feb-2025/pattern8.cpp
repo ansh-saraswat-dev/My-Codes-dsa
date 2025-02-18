7a68bf95389bafba70143f1aad26174d4824c12e
ansh-saraswat-dev <anshsaraswat000@gmail.com>
2025-02-18 19:09:50 +0530

    Revising Basics (Pattern printing for Char )

---
+++ 2-feb-2025/pattern8.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int i=1;
+    char cj;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cj='A'+ i -1;
+            cout<<cj;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+        
+    }
+    
+    return 0;
+}
+// OUTPUT 
+// 3
+// AAA
+// BBB
+// CCC
