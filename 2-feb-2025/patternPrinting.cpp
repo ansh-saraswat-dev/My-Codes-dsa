406a31936dc74ca1a7f6217cb334060043ca868c
Ansh Saraswat <anshsaraswat000@gmail.com>
2025-02-18 17:37:28 +0530

    Revising basics

--- 2-feb-2025/patternPrinting.cpp
+++ 2-feb-2025/patternPrinting.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 int main(){
-    string ansh="an";
-    cout<<ansh;
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
     return 0;
 }
+
+// OUTPUT
+// 4
+// ****
+// ****
+// ****
+// ****
