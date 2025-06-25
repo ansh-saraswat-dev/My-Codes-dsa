791f161c4307b9aae2e40a289a7f56ba0a0879a3
Ansh_Dev <anshsaraswat000@gmail.com>
2025-06-25 10:08:56 +0530

    Time: 0 ms (100%), Space: 23 MB (56.35%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -19,26 +19,38 @@ public:
     //     int r=height(root->right);
     //     return max(l,r)+1;
     // }
-    pair<bool,int> isBalancedFast(TreeNode* root){
-    if(root == NULL){
-        pair<bool,int> p= make_pair(true,0);
-            return p;
-        }
-        pair<bool,int> left=isBalancedFast(root->left);
-        pair<bool,int> right=isBalancedFast(root->right);
+    // pair<bool,int> isBalancedFast(TreeNode* root){
+    // if(root == NULL){
+    //     pair<bool,int> p= make_pair(true,0);
+    //         return p;
+    //     }
+    //     pair<bool,int> left=isBalancedFast(root->left);
+    //     pair<bool,int> right=isBalancedFast(root->right);
 
-        bool leftAns=left.first;
-        bool rightAns=right.first;
-        bool difference= abs(left.second - right.second) <= 1;
-        pair<bool,int> ans;
-        ans.second=max(left.second,right.second)+1;
-        if(leftAns && rightAns && difference){
-            ans.first = true;
-        }
-        else{
-        ans.first = false;}
-        return ans;
+    //     bool leftAns=left.first;
+    //     bool rightAns=right.first;
+    //     bool difference= abs(left.second - right.second) <= 1;
+    //     pair<bool,int> ans;
+    //     ans.second=max(left.second,right.second)+1;
+    //     if(leftAns && rightAns && difference){
+    //         ans.first = true;
+    //     }
+    //     else{
+    //     ans.first = false;}
+    //     return ans;
+    // }
+    bool isBal=true;
+    int maxDepth(TreeNode* root){
+        if(!root)
+            return 0;
+        int l=maxDepth(root->left);
+        int r=maxDepth(root->right);
+        if(abs(l-r)>1) isBal=false;
+        return max(l,r)+1;
     }
+
     bool isBalanced(TreeNode* root) {
-        return isBalancedFast(root).first;
+        // return isBalancedFast(root).first;
+        maxDepth(root);
+        return isBal;
 }};
