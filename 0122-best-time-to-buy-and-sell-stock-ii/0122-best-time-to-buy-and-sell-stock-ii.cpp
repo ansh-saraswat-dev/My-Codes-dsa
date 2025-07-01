79ae501ac181b1f336f0d8235bf76c0fb61c023d
Ansh_Dev <anshsaraswat000@gmail.com>
2025-07-01 10:06:47 +0530

    Time: 3 ms (40.54%), Space: 20 MB (20.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,27 @@
 class Solution {
+    int profit(vector<int>& prices, int day, int state, vector<vector<int>>& table) {
+        if (day == prices.size()) return 0;
+
+        if (table[state][day] != -1) return table[state][day];
+
+        int maxProfit = 0;
+        if (state == 0) {
+            int buy = -prices[day] + profit(prices, day + 1, 1, table);
+            int dontBuy = profit(prices, day + 1, 0, table);
+            maxProfit = max(buy, dontBuy);
+        } else {
+            int sell = prices[day] + profit(prices, day + 1, 0, table);
+            int dontSell = profit(prices, day + 1, 1, table);
+            maxProfit = max(sell, dontSell);
+        }
+
+        return table[state][day] = maxProfit;
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]>prices[i-1]){
-                profit += prices[i] - prices[i-1];
-            }
-        }
-        return profit;
+        int n = prices.size();
+        vector<vector<int>> table(2, vector<int>(n, -1));  // memo table
+        return profit(prices, 0, 0, table);  // start at day 0, can buy
     }
 };
