134c63395d151b5f1e32da459c54a5c10e36ffdb
Ansh_Dev <anshsaraswat000@gmail.com>
2025-08-02 13:55:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -4,17 +4,39 @@ class Solution {
         // code here
         int n1=a.size();
         int n2=b.size();
-        set<int> st;
-        for(int i=0;i<n1;i++){
-            st.insert(a[i]);
+        int i=0;
+        int j=0;
+        vector<int> unionArr;
+        while(i<n1 && j<n2){
+            if(a[i]<=b[j]){
+                if(unionArr.size()==0 ||
+                unionArr.back() != a[i]){
+                    unionArr.push_back(a[i]);
+                }
+                i++;
+            }
+            else{
+                if(unionArr.size()==0 ||
+                unionArr.back() != b[j]){
+                    unionArr.push_back(b[j]);
+                }
+                j++;
+            }
         }
-        for(int j=0;j<n2;j++){
-            st.insert(b[j]);
+        while(i<n1){
+            if(unionArr.size()==0 ||
+                unionArr.back() != a[i]){
+                    unionArr.push_back(a[i]);
+                }
+                i++;
         }
-        vector<int> temp;
-        for(auto it:st){
-            temp.push_back(it);
+        while(j<n2){
+            if(unionArr.size()==0 ||
+                unionArr.back() != b[j]){
+                    unionArr.push_back(b[j]);
+                }
+                j++;
         }
-        return temp;
+        return unionArr;
     }
 };
