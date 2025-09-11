32ddefc8b127d2f722c1f8c5afe0730c6b3db761
Ansh_Dev <anshsaraswat000@gmail.com>
2025-09-11 22:26:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -5,7 +5,7 @@ class Solution {
         int temp=arr[0];
         for(int i=0;i<arr.size();i++){
             if(arr[i]>temp){
-                temp=arr[i];
+                temp = arr[i];
             }
         }
         return temp;
