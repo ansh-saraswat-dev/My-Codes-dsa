2bf161e8f09751fb82800887bf47ffe11b7492eb
Ansh_Dev <anshsaraswat000@gmail.com>
2025-09-14 14:38:58 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size(), i = n - 2;
+
+        // Step 1: Find first decreasing element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            // Step 2: Find element to swap with
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 4: Reverse the second part
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
