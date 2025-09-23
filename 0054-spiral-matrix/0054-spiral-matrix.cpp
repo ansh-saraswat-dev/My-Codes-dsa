2a8be826098c24c1b64510891fb4de480d4d185d
Ansh_Dev <anshsaraswat000@gmail.com>
2025-09-23 18:52:30 +0530

    Time: 0 ms (100%), Space: 9.2 MB (97.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        int top = 0, bottom = m - 1;
-        int left = 0, right = n - 1;
-        
-        while (top <= bottom && left <= right) {
-            // Left to Right
-            for (int j = left; j <= right; j++) {
-                result.push_back(matrix[top][j]);
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int left=0, right=m-1;
+        int top=0, bottom=n-1;
+        vector<int> ans;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                ans.push_back(matrix[top][i]);
             }
             top++;
-
-            // Top to Bottom
-            for (int i = top; i <= bottom; i++) {
-                result.push_back(matrix[i][right]);
+            for(int i=top;i<=bottom;i++){
+                ans.push_back(matrix[i][right]);
             }
             right--;
-
-            // Right to Left (check if row remains)
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    result.push_back(matrix[bottom][j]);
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    ans.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
-
-            // Bottom to Top (check if column remains)
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.push_back(matrix[i][left]);
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(matrix[i][left]);
                 }
                 left++;
             }
         }
-        return result;
+        return ans;
     }
 };
